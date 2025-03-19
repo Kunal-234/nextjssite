0a20c9b8e99815b9109109fecfb2c1cb0a4e8978
Kunal <kunaljsr11@gmail.com>
2025-03-19 21:09:45 +0530

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,7 +8,7 @@ export default function Home() {
         </div>
         <ul className="flex justify-end gap-22">
           <li>Home</li>
-          <li>About</li>
+          <li>About me</li>
           <li>Services</li>
           <li>Contact</li>
         </ul>
