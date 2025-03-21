c7dc661e9a57675663e570dd50cafe7b2f166eaa
Kunal <kunaljsr11@gmail.com>
2025-03-21 19:42:16 +0530

    .

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,11 @@
 export default function Home() {
   return (
     <div>
-      <nav className="flex justify-between  sm:mx-0 sm:px-0 gap-22 mx-9 px-9 m-3 p-1 ">
+      <nav className="flex justify-between  sm:m-0 sm:p-0 sm:gap-0 gap-22 mx-9 px-9 m-3 p-1 ">
         <div>
           <h2 className="text-2xl"> Demo Website</h2>
         </div>
-        <ul className="flex justify-end sm:justify-start sm:gap-3 gap-22">
+        <ul className="flex justify-end sm:justify-between sm:gap-0 gap-22">
           <li>Home</li>
           <li>About </li>
           <li>Services</li>
